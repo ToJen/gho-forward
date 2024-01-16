4c45a062e2ce7768849fdacb9b7d2886810b6cb2
arowdy98 <arathore98@gmail.com>
2024-01-17 03:04:14 +0530

    Passport added to frontend.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,6 +7,8 @@ import Push from '../assets/push-logo.jpeg'
 const ethers = require("ethers")
 
 const EthInWei = 1000000000000000000;
+const API_KEY = "fKkSd21Z.e7WkHo51ArHiJor6QTOc5c2ND1j7dl9u";
+const SCORER_ID = 6351;
 
 const Home = () => {
   const [connectedAddress, setConnectedAddress] = useState(null)
@@ -26,6 +28,9 @@ const Home = () => {
   const [collateralAmount, setCollateralAmount] = useState(0)
   const [signedContract, setSignedContract] = useState(null)
   const [timeOverdue, setTimeOverdue] = useState(0);
+  const [signer, setSigner] = useState(null);
+  const [score, setScore] = useState('');
+  const [noScoreMessage, setNoScoreMessage] = useState('');
 
   const getCurrentAccount = async () => {
     const { ethereum } = window;
@@ -65,9 +70,86 @@ const Home = () => {
   useEffect(() => {
     if (connectedAddress) {
       const signer = provider.getSigner();
+      setSigner(signer);
       // setSignedContract(new ethers.Contract(contractAddress, ABI, signer))
     }
   }, [connectedAddress])
+
+  const headers = API_KEY ? ({
+    'Content-Type': 'application/json',
+    'X-API-Key': API_KEY
+  }) : undefined
+
+  // submitting passport
+  const SUBMIT_PASSPORT_URI = 'https://api.scorer.gitcoin.co/registry/submit-passport'
+  // getting the signing message
+  const SIGNING_MESSAGE_URI = 'https://api.scorer.gitcoin.co/registry/signing-message'
+
+  async function checkPassport(currentAddress = connectedAddress) {
+    console.log(`checking passport for ${currentAddress}`);
+    setScore('');
+    setNoScoreMessage('');
+
+    const GET_PASSPORT_SCORE_URI = `https://api.scorer.gitcoin.co/registry/score/${SCORER_ID}/${currentAddress}`;
+
+    try {
+      const response = await fetch(GET_PASSPORT_SCORE_URI, {
+        headers
+      })
+      const passportData = await response.json()
+      console.log('passportData: ', passportData);
+
+      if (passportData.score) {
+        const roundedScore = Math.round(passportData.score * 100) / 100
+        setScore(roundedScore.toString())
+      }
+      else {
+        console.log('No score available, please add stamps to your passport and then resubmit.')
+        setNoScoreMessage('No score available, please submit your passport after you have added some stamps.')
+      }
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
+
+  async function getSigningMessage() {
+    try {
+      const response = await fetch(SIGNING_MESSAGE_URI, {
+        headers
+      });
+      const json = await response.json();
+      return json;
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  }
+
+  async function submitPassport() {
+    setNoScoreMessage('');
+
+    if (!signer) return
+    try {
+      const { message, nonce } = await getSigningMessage()
+      const signature = await signer.signMessage(message)
+
+      console.log(signature)
+      const response = await fetch(SUBMIT_PASSPORT_URI, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
+          connectedAddress,
+          scorer: SCORER_ID,
+          signature,
+          nonce
+        })
+      })
+
+      const data = await response.json()
+      console.log('data:', data)
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
 
   return (
     <div style={{ height: '100vh' }}>
@@ -108,6 +190,11 @@ const Home = () => {
               >
                 {connectedAddress}
               </span>
+              
+              <div >
+                  <button onClick={submitPassport}>Submit Passport</button>
+                  <button onClick={() => checkPassport()}>Refresh passport score</button>
+                </div>
             </div>
           ) : (
             <button onClick={handleConnectWallet} className="wallet-button">
@@ -128,6 +215,8 @@ const Home = () => {
           <h3>Welcome to</h3>
           <h1>Uncollaterised Loans</h1>
           <h2>For Decentralised Finance</h2>
+          <h2>{score}</h2>
+          <h2>{noScoreMessage}</h2>
           <span>
             Project X tries to solve the problem of overcollateralization in DeFi.
             It uses your on-chain and off-chain information to create a trust
