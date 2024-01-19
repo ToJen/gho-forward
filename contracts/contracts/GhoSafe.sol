e5c2aebb8d1779e2acc325385fa06b702fb8d838
heypran <pranjal.baweja@gmail.com>
2024-01-20 00:40:34 +0100

    feat: gho repay

--- contracts/contracts/GhoSafe.sol
+++ contracts/contracts/GhoSafe.sol
@@ -8,16 +8,17 @@ import {IERC20WithPermit} from "@aave/core-v3/contracts/interfaces/IERC20WithPer
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
 
+// 0x50C3357Bc7608f3ac2EA301De154e122EBeAc63E CD
 // 0xa7750fe3Bbc014077672a0eDe9C7C4a554A4B6a6, 1000000000000000000,0,0,0
 contract GhoSafe is ReentrancyGuard, Ownable {
   struct BorrowRequest {
     address user;
+    address fulfilledBy;
     uint256 amount;
     uint256 interestRate;
     uint256 repayTime;
     uint256 passportScore;
     uint256 onChainScore;
-    bool isFulfilled;
     bool hasRepayed;
   }
 
@@ -37,6 +38,7 @@ contract GhoSafe is ReentrancyGuard, Ownable {
   error BorrowLimitReached(); // cannot borrow twice or increase limit
   error BorrowRequestInvalid();
   error BorrowRequestAlreadyFulfilled();
+  error NotAuthorized();
 
   // events
   event BorrowRequestCreated(uint256 indexed id, address indexed user, uint256 indexed amount);
@@ -44,7 +46,7 @@ contract GhoSafe is ReentrancyGuard, Ownable {
 
   // event CreditDelegated(address indexed delegatedBy, uint256 indexed amount, uint256 indexed deadline);
 
-  constructor(address _debtToken, address _poolAddress, address _ghoAddress) Ownable() {
+  constructor() Ownable(msg.sender) {
     DEBT_TOKEN = 0x67ae46EF043F7A4508BD1d6B94DB6c33F0915844;
     POOL_ADDRESS = 0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951;
     GHO_TOKEN = 0xc4bF5CbDaBE595361438F8c6a187bDc330539c60;
@@ -64,18 +66,27 @@ contract GhoSafe is ReentrancyGuard, Ownable {
 
     borrowRequests[borrowRequestIndex] = BorrowRequest({
       user: _user,
+      fulfilledBy: address(0),
       amount: _amount,
       repayTime: _repayTime,
       interestRate: 0, //TODO from where?
       passportScore: _passportScore,
       onChainScore: _onChainScore,
-      isFulfilled: false,
       hasRepayed: false
     });
 
     emit BorrowRequestCreated(borrowRequestIndex++, _user, _amount);
   }
 
+  // v: 28
+
+  // {r: '0x76b54720cc7e4d81d48cea39adddc1f5f57653c7d48d83c6867e972f201562e9',
+  // s: '0x77f8130be75046c67da200784e829855022a1ef1b4fdb3df3d64814dc8a4c942'
+  // CD 0x50C3357Bc7608f3ac2EA301De154e122EBeAc63E
+  // 0, 0x32D1C9FFee079d6FD8E0d0E77aD5644DDdb3d95a,1805356853,28,0xffb204e5c7b5d0884cc7fe52eeec1705b762689296f2b47efceadbc36cfaa4cc,0x14b46016a36c81168708f1e03eaf438c15a5db06c6f6a4ae64df83e3b19a7493,0xa7750fe3Bbc014077672a0eDe9C7C4a554A4B6a6,true,true
+  // _delegator: 0x32D1C9FFee079d6FD8E0d0E77aD5644DDdb3d95a
+  // dealdline: 1805356853
+  //
   // How about we create a risk score and let the lender decide based on the risk score
   function fulfillBorrowRequestWithDelegatedSig(
     uint256 borrowRequestId,
@@ -87,21 +98,26 @@ contract GhoSafe is ReentrancyGuard, Ownable {
     address _delegatee,
     bool _borrow,
     bool _transfer
-  ) external nonReentrant onlyOwner {
-    uint256 amountBorrowed = borrowRequests[borrowRequestId].amount;
-    if (amountBorrowed == 0) {
+  ) external nonReentrant {
+    if (borrowRequests[borrowRequestId].user == address(0)) {
       revert BorrowRequestInvalid();
     }
-    bool isFulfilled = borrowRequests[borrowRequestId].isFulfilled;
-    if (isFulfilled) {
+
+    if (borrowRequests[borrowRequestId].fulfilledBy != address(0)) {
       revert BorrowRequestAlreadyFulfilled();
     }
+
+    if (borrowRequests[borrowRequestId].user != msg.sender) {
+      revert NotAuthorized();
+    }
+
     // skip credit limit checks if called by owner/manager
 
     // TODO repay time check
 
-    // mark fulfilled
-    //borrowRequests[borrowRequestId].isFulfilled = true;
+    // TODO mark fulfilled
+    borrowRequests[borrowRequestId].fulfilledBy = _delegator;
+    uint256 amountBorrowed = borrowRequests[borrowRequestId].amount;
 
     ICreditDelegationToken(DEBT_TOKEN).delegationWithSig(
       _delegator,
@@ -131,6 +147,36 @@ contract GhoSafe is ReentrancyGuard, Ownable {
     emit BorrowRequestFulfilled(borrowRequestId, borrowRequests[borrowRequestId].user, _delegatee);
   }
 
+  // 0x50C3357Bc7608f3ac2EA301De154e122EBeAc63E
+  // 0, 1805356853,27,0xee9f5c50a07bc583d20c9d47bf3d0d4ffd0313ff23f0e52c409be872420f1fae, 0x4f80816b1db7151148d39baea30ecdcc314f6d99eb9bbe1c6149400ebec99ba1
+  // r: '0x6df71e0f8986f9aa4180a6b58b167e4c2be257f9e9faf03aa7c2271a163ee47c',
+  // s: '0x64029ff49e996aefa651c9fc3c360753fb33c0ccb5f814292123f64c9d2ebb08',
+  // v: 27
+  // r: '0xee9f5c50a07bc583d20c9d47bf3d0d4ffd0313ff23f0e52c409be872420f1fae', s: '0x4f80816b1db7151148d39baea30ecdcc314f6d99eb9bbe1c6149400ebec99ba1'
+  // 0,
+  function repayBorrowRequestWithPermit(
+    uint256 _borrowRequestId,
+    // address onBehalfOf,
+    uint256 _deadline,
+    uint8 _permitV,
+    bytes32 _permitR,
+    bytes32 _permitS
+  ) external nonReentrant {
+    // todo checks
+
+    IPool(POOL_ADDRESS).repayWithPermit(
+      GHO_TOKEN,
+      borrowRequests[_borrowRequestId].amount,
+      interestRateMode,
+      borrowRequests[_borrowRequestId].fulfilledBy,
+      _deadline,
+      _permitV,
+      _permitR,
+      _permitS
+    );
+  }
+
+  // TODO delete
   function fulfillBorrowRequestApprovedDelegation(
     uint256 borrowRequestId,
     address _delegator
@@ -139,10 +185,11 @@ contract GhoSafe is ReentrancyGuard, Ownable {
     if (amountBorrowed == 0) {
       revert BorrowRequestInvalid();
     }
-    bool isFulfilled = borrowRequests[borrowRequestId].isFulfilled;
-    if (isFulfilled) {
-      revert BorrowRequestAlreadyFulfilled();
-    }
+    // bool isFulfilled = borrowRequests[borrowRequestId].isFulfilled;
+
+    // if (isFulfilled) {
+    //   revert BorrowRequestAlreadyFulfilled();
+    // }
     // skip credit limit checks if called by owner/manager
 
     // TODO repay time check
@@ -165,7 +212,7 @@ contract GhoSafe is ReentrancyGuard, Ownable {
     emit BorrowRequestFulfilled(borrowRequestId, borrowRequests[borrowRequestId].user, address(this));
   }
 
-  // unprotected
+  // unprotected todo delete
   function withdrawToken(uint256 borrowRequestId) public {
     require(
       IERC20(GHO_TOKEN).transfer(borrowRequests[borrowRequestId].user, borrowRequests[borrowRequestId].amount),
