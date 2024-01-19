af804c7fc8a8debb68d26a35da9260e939118bf5
heypran <pranjal.baweja@gmail.com>
2024-01-19 21:35:44 +0100

    feat: on chain balances

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,12 @@
 // import ABI from '../contracts/LoanFactoryABI.json'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useModal } from "connectkit";
 import { useAccount, useDisconnect, useWalletClient } from "wagmi";
-import HowItWorks from '../assets/HowItWorks.png'
-import Compound from '../assets/compound-logo.png'
-import Sismo from '../assets/sismo-logo.png'
-import Push from '../assets/push-logo.jpeg'
+import HowItWorks from "../assets/HowItWorks.png";
+import Compound from "../assets/compound-logo.png";
+import Sismo from "../assets/sismo-logo.png";
+import Push from "../assets/push-logo.jpeg";
+import { getBalances } from "../utils/utils";
 
 const EthInWei = 1000000000000000000;
 const API_KEY = "fKkSd21Z.e7WkHo51ArHiJor6QTOc5c2ND1j7dl9u";
@@ -14,118 +15,125 @@ const SCORER_ID = 6351;
 const Home = () => {
   const { address, isConnecting, isConnected, isDisconnected } = useAccount();
   const { setOpen } = useModal();
-  const contractAddress = '0xC2eDd4C8fD6ae11bD209e3eE7cC0B60159A92663'
-  const GITCOIN_PASSPORT_HOLDERS = '0x1cde61966decb8600dfd0749bd371f12'
-  const ROCIFI_CREDIT_HOLDERS = '0xb3ac412738ed399acab21fbda9add42c'
+  const contractAddress = "0xC2eDd4C8fD6ae11bD209e3eE7cC0B60159A92663";
+  const GITCOIN_PASSPORT_HOLDERS = "0x1cde61966decb8600dfd0749bd371f12";
+  const ROCIFI_CREDIT_HOLDERS = "0xb3ac412738ed399acab21fbda9add42c";
 
-  const [loanAmount, setLoanAmount] = useState(0)
-  const [creditScore, setCreditScore] = useState(0)
-  const [loanInterest, setLoanInterest] = useState(0)
-  const [showLoan, setShowLoan] = useState(false)
-  const [loanExists, setLoanExists] = useState(false)
+  const [loanAmount, setLoanAmount] = useState(0);
+  const [creditScore, setCreditScore] = useState(0);
+  const [loanInterest, setLoanInterest] = useState(0);
+  const [showLoan, setShowLoan] = useState(false);
+  const [loanExists, setLoanExists] = useState(false);
   const [pendingAmount, setPendingAmount] = useState(0);
   const [healthRatio, setHealthRatio] = useState(0);
-  const [collateralAmount, setCollateralAmount] = useState(0)
-  const [signedContract, setSignedContract] = useState(null)
+  const [collateralAmount, setCollateralAmount] = useState(0);
+  const [signedContract, setSignedContract] = useState(null);
   const [timeOverdue, setTimeOverdue] = useState(0);
 
   const { data: signer } = useWalletClient();
-  const [score, setScore] = useState('');
-  const [noScoreMessage, setNoScoreMessage] = useState('');
+  const [score, setScore] = useState("");
+  const [noScoreMessage, setNoScoreMessage] = useState("");
 
   useEffect(() => {
     if (isConnected) {
-      checkPassport()
+      checkPassport();
     }
   }, [isConnected]);
 
-  const headers = API_KEY ? ({
-    'Content-Type': 'application/json',
-    'X-API-Key': API_KEY
-  }) : undefined
+  const headers = API_KEY
+    ? {
+        "Content-Type": "application/json",
+        "X-API-Key": API_KEY,
+      }
+    : undefined;
 
   // submitting passport
-  const SUBMIT_PASSPORT_URI = 'https://api.scorer.gitcoin.co/registry/submit-passport'
+  const SUBMIT_PASSPORT_URI =
+    "https://api.scorer.gitcoin.co/registry/submit-passport";
   // getting the signing message
-  const SIGNING_MESSAGE_URI = 'https://api.scorer.gitcoin.co/registry/signing-message'
+  const SIGNING_MESSAGE_URI =
+    "https://api.scorer.gitcoin.co/registry/signing-message";
 
   async function checkPassport(currentAddress = address) {
     console.log(`checking passport for ${currentAddress}`);
-    setScore('');
-    setNoScoreMessage('');
+    setScore("");
+    setNoScoreMessage("");
 
     const GET_PASSPORT_SCORE_URI = `https://api.scorer.gitcoin.co/registry/score/${SCORER_ID}/${currentAddress}`;
 
     try {
       const response = await fetch(GET_PASSPORT_SCORE_URI, {
-        headers
-      })
-      const passportData = await response.json()
-      console.log('passportData: ', passportData);
+        headers,
+      });
+      const passportData = await response.json();
+      console.log("passportData: ", passportData);
 
       if (passportData.score) {
-        const roundedScore = Math.round(passportData.score * 100) / 100
-        setScore(roundedScore.toString())
-      }
-      else {
-        console.log('No score available, please add stamps to your passport and then resubmit.')
-        setNoScoreMessage('No score available, please submit your passport after you have added some stamps.')
+        const roundedScore = Math.round(passportData.score * 100) / 100;
+        setScore(roundedScore.toString());
+      } else {
+        console.log(
+          "No score available, please add stamps to your passport and then resubmit."
+        );
+        setNoScoreMessage(
+          "No score available, please submit your passport after you have added some stamps."
+        );
       }
     } catch (err) {
-      console.log('error: ', err)
+      console.log("error: ", err);
     }
   }
 
   async function getSigningMessage() {
     try {
       const response = await fetch(SIGNING_MESSAGE_URI, {
-        headers
+        headers,
       });
       const json = await response.json();
       return json;
     } catch (err) {
-      console.log('error: ', err);
+      console.log("error: ", err);
     }
   }
 
   async function submitPassport() {
-    setNoScoreMessage('');
+    setNoScoreMessage("");
 
-    if (!signer) return
+    if (!signer) return;
     try {
-      const { message, nonce } = await getSigningMessage()
-      const signature = await signer.signMessage({message})
+      const { message, nonce } = await getSigningMessage();
+      const signature = await signer.signMessage({ message });
 
-      console.log(signature)
+      console.log(signature);
       const response = await fetch(SUBMIT_PASSPORT_URI, {
-        method: 'POST',
+        method: "POST",
         headers,
         body: JSON.stringify({
           address,
           scorer: SCORER_ID,
           signature,
-          nonce
-        })
-      })
+          nonce,
+        }),
+      });
 
-      const data = await response.json()
-      console.log('data:', data)
+      const data = await response.json();
+      console.log("data:", data);
     } catch (err) {
-      console.log('error: ', err)
+      console.log("error: ", err);
     }
   }
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <header className="App-header">
         <div style={{ paddingLeft: 20 }}>
           <span
-            style={{ fontSize: 'larger', color: '#3466AA', fontWeight: 'bold' }}
+            style={{ fontSize: "larger", color: "#3466AA", fontWeight: "bold" }}
           >
             Project
           </span>
           <span
-            style={{ fontSize: 'larger', color: '#114084', fontWeight: 'bold' }}
+            style={{ fontSize: "larger", color: "#114084", fontWeight: "bold" }}
           >
             X
           </span>
@@ -135,30 +143,39 @@ const Home = () => {
             <div>
               <span
                 style={{
-                  color: '#82B7DC',
-                  fontSize: 'medium',
-                  fontWeight: 'bold',
+                  color: "#82B7DC",
+                  fontSize: "medium",
+                  fontWeight: "bold",
                 }}
               >
-                Connected to:{' '}
+                Connected to:{" "}
               </span>
               <span
                 style={{
-                  color: 'black',
-                  fontSize: 'large',
-                  backgroundColor: '#82B7DC',
-                  borderRadius: '10px',
-                  padding: '4px 8px',
-                  color: 'white',
+                  color: "black",
+                  fontSize: "large",
+                  backgroundColor: "#82B7DC",
+                  borderRadius: "10px",
+                  padding: "4px 8px",
+                  color: "white",
                 }}
               >
                 {address}
               </span>
-              
-              <div >
-                  <button onClick={submitPassport}>Submit Passport</button>
-                  <button onClick={() => checkPassport()}>Refresh passport score</button>
-                </div>
+
+              <div>
+                <button onClick={submitPassport}>Submit Passport</button>
+                <button onClick={() => checkPassport()}>
+                  Refresh passport score
+                </button>
+                <button
+                  onClick={async () => {
+                    console.log('getBalances([""])', await getBalances([""]));
+                  }}
+                >
+                  Get Balances
+                </button>
+              </div>
             </div>
           ) : (
             <button onClick={() => setOpen(true)} className="wallet-button">
@@ -169,10 +186,10 @@ const Home = () => {
       </header>
       <div
         style={{
-          flexDirection: 'column',
-          display: 'flex',
-          width: '70vw',
-          marginTop: '10vh',
+          flexDirection: "column",
+          display: "flex",
+          width: "70vw",
+          marginTop: "10vh",
         }}
       >
         <div className="intro">
@@ -182,9 +199,9 @@ const Home = () => {
           <h2>{score}</h2>
           <h2>{noScoreMessage}</h2>
           <span>
-            Project X tries to solve the problem of overcollateralization in DeFi.
-            It uses your on-chain and off-chain information to create a trust
-            score and based on this trust score it decides whether you are
+            Project X tries to solve the problem of overcollateralization in
+            DeFi. It uses your on-chain and off-chain information to create a
+            trust score and based on this trust score it decides whether you are
             eligible for an uncollaterised loan or not. All this while
             leveraging the power of zero knowledge proofs to make sure you
             privacy is not compromised.
@@ -198,10 +215,10 @@ const Home = () => {
           <h4>Powered by:</h4>
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignContent: 'center',
-              justifyContent: 'center',
+              display: "flex",
+              flexDirection: "row",
+              alignContent: "center",
+              justifyContent: "center",
             }}
           >
             <a href="https://www.sismo.io/">
@@ -233,40 +250,40 @@ const Home = () => {
       </div>
       <div
         style={{
-          position: 'fixed',
+          position: "fixed",
           bottom: 0,
           right: 0,
-          width: '30vw',
-          height: '90vh',
+          width: "30vw",
+          height: "90vh",
         }}
       >
         <div
           style={{
-            flexDirection: 'column',
-            display: 'flex',
-            padding: '30px',
-            backgroundColor: '#F1F1F1',
-            height: 'inherit',
+            flexDirection: "column",
+            display: "flex",
+            padding: "30px",
+            backgroundColor: "#F1F1F1",
+            height: "inherit",
           }}
         >
           <div
             className="content-section"
-            style={{ color: '#82B7DC', flex: 1 }}
+            style={{ color: "#82B7DC", flex: 1 }}
           >
-            <h2>{loanExists ? 'Your Loan Details' : 'GET STARTED'}</h2>
+            <h2>{loanExists ? "Your Loan Details" : "GET STARTED"}</h2>
           </div>
 
           <div
             className="content-section"
-            style={{ color: '#82B7DC', flex: 3 }}
+            style={{ color: "#82B7DC", flex: 3 }}
           >
             {loanExists ? (
               <div
                 style={{
-                  flexDirection: 'column',
-                  display: 'flex',
-                  color: '#82B7DC',
-                  fontWeight: 'bold',
+                  flexDirection: "column",
+                  display: "flex",
+                  color: "#82B7DC",
+                  fontWeight: "bold",
                 }}
               >
                 <span className="loan-info">
@@ -276,24 +293,19 @@ const Home = () => {
                   Interest accumulated till date: {loanInterestAmount}
                 </span>
                 <span className="loan-info">Amount Paid: {amountPaid}</span> */}
+                <span className="loan-info">Health Ratio: {healthRatio}</span>
                 <span className="loan-info">
-                  Health Ratio: {healthRatio}
+                  Amount pending to be paid: {pendingAmount}
                 </span>
-                <span className="loan-info">
-                  Amount pending to be paid:{' '}
-                  {pendingAmount}
-                </span>
-                <span className="loan-info">
-                  Time Overdue: {timeOverdue}
-                </span>
+                <span className="loan-info">Time Overdue: {timeOverdue}</span>
               </div>
             ) : showLoan ? (
               <div
                 style={{
-                  flexDirection: 'column',
-                  display: 'flex',
-                  color: '#82B7DC',
-                  fontWeight: 'bold',
+                  flexDirection: "column",
+                  display: "flex",
+                  color: "#82B7DC",
+                  fontWeight: "bold",
                 }}
               >
                 <span className="loan-description">
@@ -312,7 +324,7 @@ const Home = () => {
                 <span className="loan-description">
                   {collateralAmount
                     ? "You don't have the required trust score yet for a collateral free loan"
-                    : 'Yay! You are eligible for a collateral free loan'}
+                    : "Yay! You are eligible for a collateral free loan"}
                 </span>
               </div>
             ) : (
@@ -325,10 +337,10 @@ const Home = () => {
               </p>
             )}
           </div>
-        </div>  
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,126 @@
+import { ERC20_ABI, allChainTokenInfo, tokenRegistry } from "./tokenRegistry";
+import { createPublicClient, formatEther, formatUnits, http } from "viem";
+import {
+  polygon,
+  mainnet,
+  arbitrum,
+  optimism,
+  bsc,
+  base,
+  linea,
+  zkSync,
+} from "viem/chains";
+
+const supportChains = [
+  polygon,
+  mainnet,
+  arbitrum,
+  optimism,
+  base,
+  linea,
+  zkSync,
+];
+
+function getPublicClient(supportChain) {
+  return createPublicClient({
+    chain: supportChain,
+    transport: http(),
+  });
+}
+//takes in multiple address
+export async function getBalances(addresses) {
+  const finalBalances = [];
+  for (let i = 0; i < addresses.length; ++i) {
+    const currentAddr = addresses[i];
+    if (currentAddr == null) {
+      continue;
+    }
+    const allBalancesOfCurrentAddr = {};
+    // let allTokens: any[] = [];
+    for (let j = 0; j < supportChains.length; ++j) {
+      const client = getPublicClient(supportChains[j]);
+
+      // await client.createContractEventFilter({
+      //   abi: parseAbi([
+      //     'event Transfer(address indexed, address indexed, uint256)',
+      //   ]),
+      //   strict: true,
+      //   eventName: 'Transfer',
+      //   args: {
+      //     to: ['0x'],
+      //   },
+      // });
+
+      console.log(` supportChains[j]`, supportChains[j]?.network);
+      // await getAllTransferToEventsForAddr(client, currentAddr);
+
+      const nativeBalance = await client.getBalance({ address: currentAddr });
+
+      const formatBal = formatEther(nativeBalance);
+      console.log("supportChains[j]?.network!", supportChains[j]?.network);
+      const chainTokenAddresses =
+        tokenRegistry[supportChains[j]?.network]?.filter((t) => t.isEnabled) ??
+        [];
+
+      if (supportChains[j]?.network === "homestead") {
+        console.log({ chainTokenAddresses });
+      }
+      const chainTokens = [];
+
+      for (let j = 0; j < chainTokenAddresses.length; ++j) {
+        const currentToken = chainTokenAddresses[j];
+
+        await new Promise((f) => setTimeout(f, 100));
+        const tokenBal = await client.readContract({
+          address: currentToken.address,
+          functionName: "balanceOf",
+          args: [currentAddr],
+          abi: ERC20_ABI,
+        });
+        // BigInt(tokenBal) == BigInt(0)
+        if (tokenBal == 0) {
+          continue;
+        }
+
+        const tokenInfo = allChainTokenInfo.filter(
+          (t) =>
+            t.contractAddress == currentToken.address &&
+            t.chainId == supportChains[j]?.id
+        );
+
+        if (tokenInfo.length == 0) {
+          console.log(
+            "Token info not found for token address",
+            currentToken.address
+          );
+          continue;
+        }
+        const tokenDetails = tokenInfo[0];
+
+        chainTokens.push({
+          name: tokenDetails.name,
+          symbol: tokenDetails.symbol,
+          balance: formatUnits(tokenBal, tokenDetails.decimals),
+          decimals: tokenDetails.decimals,
+          contractAddress: currentToken.address,
+          networkName: supportChains[j]?.network,
+          chainId: supportChains[j]?.id,
+        });
+      }
+      // allTokens = [...allTokens, ...chainTokens];
+      // no token balances
+      if (nativeBalance == 0 && chainTokens.length === 0) {
+        continue;
+      }
+      allBalancesOfCurrentAddr[supportChains[j]?.id] = {
+        address: currentAddr,
+        networkName: supportChains[j]?.network,
+        balance: formatBal.toString(),
+        tokens: chainTokens,
+      };
+    }
+
+    finalBalances.push(allBalancesOfCurrentAddr);
+  }
+  return finalBalances;
+}
