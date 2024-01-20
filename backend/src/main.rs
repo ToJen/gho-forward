2809964a32c8ad33b11196c09b625876a1f5eb6d
Tomisin Jenrola <tomisinjenrola@gmail.com>
2024-01-20 17:34:40 -0500

    enable cors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,7 +3,8 @@ mod handlers;
 mod models;
 mod schema;
 
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http, web, App, HttpServer};
 use database::Database;
 use handlers::{get_passport_score, get_signatures_handler, signatures_handler, submit_passport};
 
@@ -12,7 +13,14 @@ async fn main() -> std::io::Result<()> {
     let db = web::Data::new(Database::new());
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
+            .wrap(cors)
             .app_data(db.clone()) // Pass the db pool to the handlers
             // .service(web::resource("/chat").route(web::post().to(chat_handler)))
             .service(
